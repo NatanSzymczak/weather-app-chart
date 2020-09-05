62b673a3ddd340310a2d93b6ccc5f3c7f1b840a8
Natan Szymczak <natan.szymczak@gmail.com>
2020-09-05 23:33:44 +0200

    Repair the NotFound component.

---
+++ src/Components/NotFound/NotFound.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
+import './NotFound.css'
+
+
+function NotFound( {location: { pathname } = "HH" } ) {
+  const [counter, setCounter] = useState(4);
+  const countdown = () => setCounter(counter-1);
+
+  useEffect(() => {
+    const intervalId = setInterval(countdown, 1250);
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, [counter]);
+
+  return (
+    <section>
+      <div className="container notFound">
+        <h3>No match for <code>{pathname}</code></h3>
+        <h3>Redirect to homepage in {counter}</h3>
+        { !counter && <Redirect to='/' /> }
+      </div>
+    </section>
+  );
+}
+
+export default NotFound;
